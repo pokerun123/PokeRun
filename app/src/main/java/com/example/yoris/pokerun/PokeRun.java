145ac914d0bb71d88c6a625ed70c5e5cf134ae9f
yoris <lorenzosalmaso20000@gmail.com>
2019-01-27 14:10:44 +0100

    pokemon

---
+++ app/src/main/java/com/example/yoris/pokerun/PokeRun.java
@@ -0,0 +1,13 @@
+package com.example.yoris.pokerun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PokeRun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poke_run);
+    }
+}
